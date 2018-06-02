c46737a021ddc7391dd0371dfe191bfbe8fd8567
MaxDMT16 <maxdmt1638@gmail.com>
2018-06-02 21:06:57 +0300

    add views for teachers

--- app/src/main/java/com/dmt/max/schoolschedule/teachers/views/details/TeacherDetailsView.java
+++ app/src/main/java/com/dmt/max/schoolschedule/teachers/views/details/TeacherDetailsView.java
@@ -1,8 +1,16 @@
 package com.dmt.max.schoolschedule.teachers.views.details;
 
+import com.dmt.max.schoolschedule.model.teachers.responses.TeacherResponse;
+
 /**
  * Created by Max on 02.06.2018.
  */
 
 public interface TeacherDetailsView {
+    void onCreateTeacherSuccess();
+    void onUpdateTeacherSuccess();
+    void onRequestFail(String message);
+    void onRequestTeacherByIdSuccess(TeacherResponse teacherResponse);
+    String getAccessToken();
+    String getRefreshToken();
 }
--- app/src/main/java/com/dmt/max/schoolschedule/teachers/views/listing/TeachersListingView.java
+++ app/src/main/java/com/dmt/max/schoolschedule/teachers/views/listing/TeachersListingView.java
@@ -1,8 +1,25 @@
 package com.dmt.max.schoolschedule.teachers.views.listing;
 
+import com.dmt.max.schoolschedule.model.teachers.Teacher;
+
+import java.util.List;
+
 /**
  * Created by Max on 02.06.2018.
  */
 
 public interface TeachersListingView {
+    void loadingStarted();
+
+    void showTeachers(List<Teacher> teachers);
+
+    void loadingFailed(String errorMessage);
+
+    void deleteTeacher(String teacherId);
+
+    void onDeleteSuccess();
+
+    void onDeleteFailed(String message);
+
+    void onTeacherClick(String teacherId);
 }
